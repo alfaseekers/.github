d797c90155b02160a54bf47e7e6c6109035b6e58
Samuel Anzalone <63590916+anzamuel@users.noreply.github.com>
2025-11-13 15:16:07 +0100

    Disable subject-case rule in commitlint config

--- .github/configs/commitlint.config.mjs
+++ .github/configs/commitlint.config.mjs
@@ -1,13 +1,6 @@
 export default {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'subject-case': [
-      2,
-      'always',
-      [
-        'lower-case',
-        'sentence-case'
-      ],
-    ],
+    'subject-case': [0,'always'],
   },
 };
